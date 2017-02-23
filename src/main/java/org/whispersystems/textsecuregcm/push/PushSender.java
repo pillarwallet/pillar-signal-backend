4f459ddc516b63d53dd0b70345449c54f2276bfa
Fay Montage <accounts@faymontage.com>
2017-02-23 13:21:25 +0100

    Add mutable-content flag to APN notification

--- src/main/java/org/whispersystems/textsecuregcm/push/PushSender.java
+++ src/main/java/org/whispersystems/textsecuregcm/push/PushSender.java
@@ -41,7 +41,7 @@ public class PushSender implements Managed {
 
   private final Logger logger = LoggerFactory.getLogger(PushSender.class);
 
-  private static final String APN_PAYLOAD = "{\"aps\":{\"sound\":\"default\",\"badge\":%d,\"alert\":{\"loc-key\":\"APN_Message\"}}}";
+  private static final String APN_PAYLOAD = "{\"aps\":{\"mutable-content\":1,\"sound\":\"default\",\"badge\":%d,\"alert\":{\"loc-key\":\"APN_Message\"}}}";
 
   private final ApnFallbackManager         apnFallbackManager;
   private final PushServiceClient          pushServiceClient;
