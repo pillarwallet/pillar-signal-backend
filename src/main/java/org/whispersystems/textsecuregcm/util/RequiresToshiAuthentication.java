829c3ed4a1cd58d067d3001d7f89c11d7ed29729
Tristan King <tristan@bakkenbaeck.no>
2017-07-03 16:41:34 +0200

    add middleware for toshi authentication

---
+++ src/main/java/org/whispersystems/textsecuregcm/util/RequiresToshiAuthentication.java
@@ -0,0 +1,13 @@
+package org.whispersystems.textsecuregcm.util;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.ws.rs.NameBinding;
+
+@NameBinding
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RequiresToshiAuthentication {}
