599f63a081a108d05097a76cdb912ba5caf085c7
Deimantas Špučys <as.deimis@gmail.com>
2018-11-30 18:49:17 +0200

    added user agent in request, fixed request response handler

--- src/main/java/org/whispersystems/textsecuregcm/microservices/CorePlatform.java
+++ src/main/java/org/whispersystems/textsecuregcm/microservices/CorePlatform.java
@@ -1,9 +1,10 @@
 package org.whispersystems.textsecuregcm.microservices;
 
-import org.apache.log4j.Logger;
 import org.eclipse.jetty.util.log.Log;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.whispersystems.textsecuregcm.controllers.MessageController;
 import org.whispersystems.textsecuregcm.controllers.NoSuchUserException;
@@ -17,11 +18,14 @@ import java.net.URL;
 
 public class CorePlatform {
 
+    private final Logger logger = LoggerFactory.getLogger(CorePlatform.class);
+
     public static final String CONNECTION_STATE_ACCEPTED = "STATE_ACCEPTED";
     public static final String CONNECTION_STATE_BLOCKED = "STATE_BLOCKED";
     public static final String CONNECTION_STATE_MUTED = "STATE_MUTED";
 
     public static final String ERROR_CORE_PLATFORM_FAILED = "ERROR_CORE_PLATFORM_FAILED";
+    public static final String ERROR_CORE_CONNECTION_FAILED = "ERROR_CORE_CONNECTION_FAILED";
 
     private String state;
     private String corePlatformUrl;
@@ -35,14 +39,14 @@ public class CorePlatform {
         void onError(String code);
     }
 
-    public void getConnectionState(String receiverId, String connectionAccessKey, Callback cb) {
+    public void getConnectionState(String receiverId, String connectionAccessKey, Callback cb) throws NoSuchUserException {
         try {
             URL url = new URL(String.format("%s/connection?userId=%s&accessKey=%s", corePlatformUrl, receiverId, connectionAccessKey));
             HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
             connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+            connection.setRequestProperty("User-Agent", "Signal-Java-Backend");
             if (connection.getResponseCode() != 200){
-                cb.onError(ERROR_CORE_PLATFORM_FAILED);
+                cb.onError(ERROR_CORE_CONNECTION_FAILED);
                 return;
             }
             BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
@@ -53,11 +57,12 @@ public class CorePlatform {
             }
             br.close();
             connection.disconnect();
-            JSONObject response = new JSONObject(jsonResponse.toString());
+            JSONArray response = new JSONArray(jsonResponse.toString());
             String state = CONNECTION_STATE_BLOCKED;
-            LoggerFactory.getLogger(MessageController.class).error(response.toString());
-            if (!response.isNull("status")){
-                switch (response.getString("status")){
+            if (response.isNull(0)) throw new IOException();
+            JSONObject connectionData = response.optJSONObject(0);
+            if (!connectionData.isNull("status")){
+                switch (connectionData.getString("status")){
                     case "muted":
                         state = CONNECTION_STATE_MUTED;
                         break;
@@ -67,7 +72,7 @@ public class CorePlatform {
                 }
             }
             cb.onSuccess(state);
-        } catch (IOException | JSONException | NoSuchUserException e) {
+        } catch (IOException | JSONException e) {
             cb.onError(ERROR_CORE_PLATFORM_FAILED);
             e.printStackTrace();
         }
